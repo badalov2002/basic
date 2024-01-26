3a8a45d57a3a55131d6254c500eac07f2fbd283b
Abdul-Aziz <100416995+badalov2002@users.noreply.github.com>
2024-01-26 19:52:21 +0500

    new changes

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -43,7 +43,7 @@ class SiteController extends Controller
             return false;
         }
         if (Yii::$app->user->isGuest) {
-            return $this->redirect('site/login');
+            return $this->redirect('login');
         }
         return true;
     }
